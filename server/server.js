24c326fe5c70ef1e7bde3a52ca7d7705eca695e1
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-28 11:23:08 -0200

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -54,6 +54,25 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  
+  Todo.findByIdAndRemove(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch((error) => {
+      res.status(404).send();
+    });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
